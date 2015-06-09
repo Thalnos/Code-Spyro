e121b6ed7c2b82f2931d757169c5bf4a166f193d
Thalnos <peter.timmerman0@gmail.com>
2015-06-09 13:32:00 +0200

    ImageFULL
    Full Image for Galileo

--- Image/startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-ifconfig wlan0 down
-iwconfig wlan0 mode ad-hoc
-iwconfig wlan0 essid RobotNetwerk
-iwconfig wlan0 channel 10
-ifconfig wlan0 192.168.1.2 netmask 255.255.255.0 up
-nice -n -20 python /pythonprograms/robotsocketsplinenewRLS.py &
-
